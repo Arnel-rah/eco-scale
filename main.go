cf21892de0a488fedde9c1b7cefb875aa88e02ae
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-05-18 18:12:17 +0300

    feat: implement container stop action triggered by policy engine

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Arnel-rah/eco-scale/config"
 	"github.com/Arnel-rah/eco-scale/docker"
+	"github.com/Arnel-rah/eco-scale/scheduler"
 )
 
 func main() {
@@ -19,12 +20,6 @@ func main() {
 	fmt.Printf("Configuration version %s chargée avec succès !\n", cfg.Version)
 	fmt.Printf("Mode alerte actuel : %v\n\n", cfg.AlertMode)
 
-	fmt.Println("Politiques définies :")
-	for _, p := range cfg.Policies {
-		fmt.Printf("- %s (Priorité: %s, CPU Max: %d%%)\n", p.ContainerName, p.Priority, p.CPULimit)
-	}
-	fmt.Println()
-
 	scanner, err := docker.NewDockerScanner()
 	if err != nil {
 		log.Fatalf("Erreur Docker: %v", err)
@@ -36,18 +31,31 @@ func main() {
 		log.Fatalf("Erreur lors de la récupération des conteneurs: %v", err)
 	}
 
-	fmt.Println("=== Conteneurs en cours d'exécution ===")
-	if len(activeContainers) == 0 {
-		fmt.Println("Aucun conteneur actif trouvé.")
+	fmt.Println("=== Analyse du Système ===")
+	actionsRequired := scheduler.AnalyzeSystem(cfg.Policies, activeContainers, cfg.AlertMode)
+
+	if len(actionsRequired) == 0 {
+		fmt.Println("Aucune action requise.")
 		return
 	}
 
-	for _, c := range activeContainers {
-		fmt.Printf("ID: %s | Nom: %s | Image: %s | Statut: %s\n",
-			c.ID[:10],
-			c.Names[0],
-			c.Image,
-			c.Status,
+	for _, target := range actionsRequired {
+		fmt.Printf("[CIBLE TROUVÉE] Conteneur: %s (%s) | Action requise: %s\n",
+			target.Name,
+			target.ID,
+			target.Required,
 		)
+
+		if target.Required == scheduler.ActionStop {
+			fmt.Printf("-> Arrêt du conteneur %s en cours...\n", target.Name)
+			err := scanner.StopContainer(target.ID)
+			if err != nil {
+				fmt.Printf("Erreur lors de l'arrêt de %s: %v\n", target.Name, err)
+			} else {
+				fmt.Printf("Conteneur %s arrêté avec succès pour économie d'énergie.\n", target.Name)
+			}
+		} else if target.Required == scheduler.ActionScale {
+			fmt.Printf("-> Action SCALE requise pour %s (%d%% CPU). (Cgroups à implémenter)\n", target.Name, target.Policy.CPULimit)
+		}
 	}
 }
